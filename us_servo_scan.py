50b9a2d2b6c838cdb85a213b507c7cb71169f9f9
Dan Adiletta <dan.adiletta@gmail.com>
2015-07-23 14:34:15 -0400

    Update us_servo_scan.py

---
+++ us_servo_scan.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+############################################################################################                                                                
+#      EDITED BY DADILETTA                           
+############################################################################################
+#
+# ! Attach Ultrasonic sensor to A1 Port.
+#
+############################################################################################
+from gopigo import *
+import sys
+from collections import Counter
+import math
+
+continue = true
+sweep = [None] * 130
+count = 0 
+sdistance = 50 
+edistance = 10
+
+def scan():
+	for ang in range(30, 130, 2):
+		servo(ang)
+		time.sleep(.02)
+		sweep[ang] = us_servo(15)
+		if sweep[ang] < edistance:
+			print("EMERGENCY STOP")
+			stop()
+
+def pscan():
+	for ang in range(30, 130, 2):
+		print('Angle of', ang, "has distance," sweep[ang])
+
+def rollon():
+	for ang in range(30, 130, 2): 
+		if sweep[ang] < sdistance:
+			print("Problem detected. Calling findavector")
+			findavector()
+			break
+		else:
+			print("Path clear, moving forward")
+			fwd()
+
+
+def findavector():
+	count = 0
+	for ang in range(30, 130, 2):
+		if sweep[ang] > ddistance:
+			count += 1
+		if count > 20:
+			if ang < 80:
+				print("Looks like I've got a path to the right.")
+			else: 
+				print("Looks like I've got a path to the left.")
+			turnto(ang)
+	if count < 20:
+		print("I don't see a path ahead.")
+		continue = false
+	
+def turnto(ang):
+	diff = 80 - ang
+	if diff >= 0:
+		stop()
+		right()
+		time.sleep(diff/1000)
+		stop()
+	if diff < 0:
+		stop()
+		left()
+		time.sleep(abs(diff)/1000)
+		stop()
+
+while continue:
+	scan()
+	pscan()
+	rollon()
+  
+stop()
+disable_servo()
