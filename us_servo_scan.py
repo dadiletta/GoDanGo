da8ebd37216c672fbbdfe9d55cd3fd5073897edc
Dan Adiletta <dan.adiletta@gmail.com>
2015-07-23 16:37:20 -0400

    Update us_servo_scan.py

--- us_servo_scan.py
+++ us_servo_scan.py
@@ -11,69 +11,58 @@ import sys
 from collections import Counter
 import math
 
-keepgoing = True
 sweep = [None] * 130
-count = 0 
-sdistance = 50 
-edistance = 10
+sdistance = 50
 
 def scan():
 	stop()
+	allclear = True
 	for ang in range(30, 130, 2):
 		servo(ang)
 		time.sleep(.02)
 		sweep[ang] = us_dist(15)
 		print("Angle of", ang, "has distance", sweep[ang])
-		if sweep[ang] < edistance:
-			print("Stop signal sent. Close obstacle.")
-			stop()
+		if sweep[ang] < sdistance:
+			allclear = False
+	return allclear
 
-def findavector():
-	count = 0
-	for ang in range(30, 130, 2):
-		if sweep[ang] > sdistance:
-			count += 1
-		if count > 20:
-			if ang < 80:
-				print("Looks like I've got a path to the right.")
-			else: 
-				print("Looks like I've got a path to the left.")
-			turnto(ang)
-	if count < 20:
-		print("I don't see a path ahead.")
-		keepgoing = False
-		print("The while loop has now been set to", keepgoing)
-	
 def turnto(ang):
 	diff = 80 - ang
+	
 	if diff >= 0:
 		stop()
+		enc_tgt(1,0,4)
 		right()
-		time.sleep(diff/1000)
+	else:
 		stop()
-	if diff < 0:
-		stop()
+		enc_tgt(0,1,4)
 		left()
-		time.sleep(abs(diff)/1000)
-		stop()
 
-def rollon():
-	for ang in range(30, 130, 2): 
-		if sweep[ang] < sdistance:
-			print("Problem detected. Calling findavector")
-			findavector()
+
+
+while True:
+	if scan() == True:
+		while True:
+			dist=us_dist(15)			#Find the distance of the object in front
+			print "Dist:",dist,'cm'
+			if dist<sdistance:	#If the object is closer than the "distance_to_stop" distance, stop the GoPiGo
+				print "Something in my way."
+				stop() #Stop the GoPiGo
+				break
+	else:
+		count = 0
+		for ang in range(30, 130, 2):
+			if sweep[ang] > sdistance:
+				count += 1
+			if count > 20:
+				if ang < 80:
+					print("Looks like I've got a path to the right.")
+				else: 
+					print("Looks like I've got a path to the left.")
+				turnto(ang)
+		if count < 20:
+			print("I don't see a path ahead.")
 			break
-		else:
-			print("Path clear, moving forward")
-			servo(80)
-			fwd()
 
-while keepgoing:
-	print("Hi. While loop is set to", keepgoing)
-	scan()
-	rollon()
-	if us_dist(15) < edistance:
-		stop()
-  
 stop()
 disable_servo()
