1c0bdb05cd0b62b25ae288996375acecdd19b589
Dan Adiletta <dan.adiletta@gmail.com>
2015-07-27 14:13:25 -0400

    Update us_servo_scan.py

--- us_servo_scan.py
+++ us_servo_scan.py
@@ -48,10 +48,11 @@ def scan():
 				allclear = False
 	return allclear
 
-def turnto(ang):
-	#TODO: Debug, sometimes not turning, sometimes turning excessively
+def turnto(ang):   #first calculate whether to use a low/med/high turn, then execute the turn
+	while stop() == None:  #stop loop to prepare for turn
+			print "Having trouble stopping"
 	diff = 80 - (ang-15)  #for some reason, 80 degrees is straight ahead with my servo. I take off 15 from ang to find the center of the window
-	turnnum = 5
+	turnnum = 5  #reset the turn num to default value
 	if abs(diff) > 30 and abs(diff) <= 60: #greater than 30 degrees, we should increase the amount needed to turn
 		turnnum = 10
 		print "Setting turn variable to 10."
@@ -59,38 +60,18 @@ def turnto(ang):
 		turnnum = 15
 		print "Setting turn variable to 15."
 	else:
-		turnnum = 5
 		print "Setting turn variable to 5."
 	if diff >= 0:
-		while stop() == 0:
-			print "Having trouble stopping"
-		print("Moving right.") 
 		enc_tgt(1,0,turnnum)
-		right()
-		print "I think I turned. Continue or retry? [c/r]"
-		answer = raw_input()
-		if answer == "r":
-			enc_tgt(1,0,turnnum)
-			right()
-		while stop() == 0:
-			print "Having trouble stopping"
+		print "Moving right. Command returned:", right(),"message"
 	else:
-		while stop() == 0:
-			print "Having trouble stopping"
-		print("Moving left.")
-		while True:
-			enc_tgt(0,1,turnnum) 
-			if left() == 1:
-				print "Left command returned as a success."
-				break
-			else:
-				print "Error turning left. Command returned a 0."
-				print "Paused. Press any key to retry."
-				raw_input()
-		while stop() == 0:
-			print "Having trouble stopping"
+		enc_tgt(0,1,turnnum) 
+		print "Moving left. Command returned:", left(),"message"
+	time.sleep(1)  #give it a second...
+	while stop() == None:
+		print "Having trouble stopping"
 
-def voltcheck():
+def voltcheck():  #this check runs at the top of the main while loop
 	if volt() < 7:
 		print "Not enough power"
 		return False
@@ -102,25 +83,18 @@ def voltcheck():
 		return True
 
 def turnaround():
-	command = raw_input().lower() #take a command and make it lowercase
-	if command == "yes" or command == "y" or command == "sure":
-		while stop() == 0:
-			print "Having trouble stopping"
-		servo(80)
-		disable_servo()
-		print "Backing up. Beep beep beep."
-		while bwd() == 0:
-			print "Having trouble backing up"
-		time.sleep(.8)  #TODO: Replace sleeps with enc_tgt. Was having trouble with it.
-		while stop() == 0:
-			print "Having trouble stopping"
-		right_rot()
-		time.sleep(.8)
-		while stop() == 0:
-			print "Having trouble stopping"
-		return True
-	else:
-		return False #user said not to continue. Return false and break the loop
+	while stop() == None:
+		print "Having trouble stopping"
+	print "Backing up. Beep beep beep."
+	while bwd() == None:
+		print "Having trouble backing up"
+	time.sleep(.8)  #TODO: Replace sleeps with enc_tgt. Was having trouble with it.
+	while stop() == None:
+		print "Having trouble stopping"
+	right_rot()
+	time.sleep(.8)
+	while stop() == None:
+		print "Having trouble stopping"
 
 #HERE'S WHERE THE PROGRAM STARTS
 while voltcheck():  #keep looping as long as the power is within acceptable range
@@ -130,7 +104,7 @@ while voltcheck():  #keep looping as long as the power is within acceptable rang
 		while True:
 			#TODO: servo sometimes twitches while driving. Why? I disable it... 
 			servo(80)  #move the sensor straight ahead, happens to be 80 for my servo
-			while disable_servo() == 0:
+			while disable_servo() == None:
 				print "Having trouble disabling my servo"
 			set_left_speed(120)  #adjust these so your GoPiGo cruises straight
 			set_right_speed(155) #adjust these so your GoPiGo cruises straight
@@ -142,23 +116,28 @@ while voltcheck():  #keep looping as long as the power is within acceptable rang
 				print "Is that something in my way?"
 			if stopcount > 2:
 				print "Yup. Something's in my way."
-				while stop() == 0:
+				while stop() == None:
 					print "Having trouble stopping"
 				break #stop the fwd loop
 	else:   #here's where we find a safe window to drive forward
-		count = 0  #
+		count = 0  #counter to track the number of safe angles in a row
 		for ang in range(10, 160, 2):
 			if sweep[ang] > fardistance:
 				count += 1   #count how many angles have a clear path ahead
 			else: 
-				count = 0   #resets the counter to 0 if a obstacle is detected, we only want 20 returns of safe in a row
+				count = 0   #resets the counter to 0 if a obstacle is detected, we only want counts in a row
 			if count >= 10:   #10 counts means 20 degrees (since I count by 2s in the loop)
 				turnto(ang)
 				break #once we've found a path, stop looping through the scan data. This favors the right side since that's scanned first
 		if count < 10:     #This is what happens if a window of obstacle-free scan data is not found
-			print("I don't see a path ahead. Shall I try a 180?")
-			if not turnaround(): #if turnaround returns false
-				break #shut it down if ya can't turn 'round
+			print "I don't see a path. [S]can again | [T]urn around | [Q]uit"
+			command = raw_input().lower() #take a command and make it lowercase
+			if command == "s" or command == "scan":
+				continue
+			elif command == "t" or command == "turn":
+				turnaround()
+			else:	
+				break
 
 stop()   #once the loop is broken, let's tidy things up just to be sure.
 disable_servo()
